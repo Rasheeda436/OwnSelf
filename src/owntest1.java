0d20e44f5390188e42a9545bb2c09915aa4a7aa6
Rasheeda436 <rasheedamohammed1@gmail.com>
2025-06-12 18:15:58 +0530

    Hello class updated

--- src/owntest1.java
+++ src/owntest1.java
@@ -1,6 +1,6 @@
 public class owntest1 {
     public static void main(String[] args) {
         System.out.println("test by self");
-        System.out.println(2+2);
+        System.out.println(2+5);
     }
 }
